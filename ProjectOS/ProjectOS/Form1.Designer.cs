32199dbe65100c81a36fc477a4dfcd91b30da3ee
geomimo <35368032+geomimo@users.noreply.github.com>
2019-01-19 17:17:07 +0200

    New

--- ProjectOS/ProjectOS/Form1.Designer.cs
+++ ProjectOS/ProjectOS/Form1.Designer.cs
@@ -39,9 +39,10 @@
             // StartBtn
             // 
             this.StartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.StartBtn.Location = new System.Drawing.Point(10, 11);
+            this.StartBtn.Location = new System.Drawing.Point(13, 14);
+            this.StartBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StartBtn.Name = "StartBtn";
-            this.StartBtn.Size = new System.Drawing.Size(95, 41);
+            this.StartBtn.Size = new System.Drawing.Size(127, 50);
             this.StartBtn.TabIndex = 0;
             this.StartBtn.Text = "Start";
             this.StartBtn.UseVisualStyleBackColor = true;
@@ -54,20 +55,20 @@
             // richTextBox1
             // 
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.richTextBox1.Location = new System.Drawing.Point(9, 57);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 70);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(1055, 384);
+            this.richTextBox1.Size = new System.Drawing.Size(1405, 472);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(110, 21);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Location = new System.Drawing.Point(147, 26);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.progressBar1.Maximum = 500;
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(180, 19);
+            this.progressBar1.Size = new System.Drawing.Size(240, 23);
             this.progressBar1.Step = 1;
             this.progressBar1.TabIndex = 2;
             // 
@@ -75,22 +76,22 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label1.Location = new System.Drawing.Point(408, 17);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(544, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(173, 24);
+            this.label1.Size = new System.Drawing.Size(216, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "TOTAL FAULTS: ";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 450);
+            this.ClientSize = new System.Drawing.Size(1576, 554);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.StartBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- ProjectOS/ProjectOS/Form1.cs
+++ ProjectOS/ProjectOS/Form1.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProjectOS
@@ -25,6 +21,7 @@ namespace ProjectOS
             InitializeComponent();
         }
 
+        //Locate form and richTextBox1.
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Location = new Point(0, 0);
@@ -32,46 +29,58 @@ namespace ProjectOS
             richTextBox1.Size = new Size(this.Width - 100, this.Height - 100);
         }
 
+        //Generate the sequence.
         private void GenerateSeq()
         {  
             Random r = new Random();
             text += Environment.NewLine + "SEQUENCE: ";
+
+            // From memory/disk take all current pages, select their IDs, suffle them (OrderBy Guid) and return that list. 
+            List<int> randMemList = memory.Take(pagesInMem).Select(x => x.ID).OrderBy(a => Guid.NewGuid()).ToList();
+            List<int> randDiskList = disk.Select(x => x.ID).OrderBy(a => Guid.NewGuid()).ToList();
+
             for(int i = 0; i < 20; i++)
             {
-                int rIndex;
-                if (r.Next(100) < 60 && pagesInMem > 19)
+                //select a page from memory with 60% probability
+                if (r.Next(100) < 60 && pagesInMem != 0)
                 {
-                    do
-                    {
-                        rIndex = r.Next(pagesInMem);
-                    } while (sequence.Contains(rIndex));
-                    sequence[i] = memory[rIndex].ID;
+                    sequence[i] = randMemList.First();
+                    randMemList.RemoveAt(0);
                 }
+                //select a page from disk with 40% probability
                 else
                 {
-                    do
+                    while (sequence.Contains(randDiskList.First()))
                     {
-                        rIndex = r.Next(128);
-                    }while (memory.Take(pagesInMem).Select(x=>x.ID).Contains(disk[rIndex].ID) || sequence.Contains(disk[rIndex].ID));
-                    
-                    sequence[i] = disk[rIndex].ID;
+                        randDiskList.RemoveAt(0);
+                    }
+                    sequence[i] = randDiskList.First();
+                    randDiskList.RemoveAt(0);
                 }
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
                 text += sequence[i].ToString() + " ";
             }
             text += Environment.NewLine;
         }
 
+        //Find the Page from memory with the minimum counter.
         private Page FindMin()
         {
             int min = Int32.MaxValue;
             Page minPage = null;
             for(int i = 0; i < pagesInMem; i++)
             {
-                int counter = sequence.Contains(memory[i].ID) && memory[i].Counter[0] ? 128 : 0;
+                //Calculate the counter.
+                int counter = 0;
                 for (int j = 0; j < 8; j++)
                 {
                     counter += memory[i].Counter[j] ? (int)Math.Pow(2D, (double)(7 - j)) : 0;
                 }
+                MessageBox.Show(counter.ToString());
+                //Find minimum.
                 if (min > counter)
                 {
                     min = counter;
@@ -81,13 +90,16 @@ namespace ProjectOS
             return minPage;
         }
 
+        //Initialising variables and start simulation.
         private void StartBtn_Click(object sender, EventArgs e)
         {
+            StartBtn.Enabled = false;
             totalFaults = 0;
             currentTime = 0;
             pagesInMem = 0;
-            text = "";
             progressBar1.Value = 0;
+            richTextBox1.Text = String.Empty;
+            text = String.Empty;
             label1.Text = "TOTAL FAULTS: ";
 
             disk = new Page[128];
@@ -96,13 +108,16 @@ namespace ProjectOS
             Page.Fill(disk);
             Random r = new Random();
 
-            Thread.Sleep(500);
+            //Just a pause, before starting.
+            Thread.Sleep(250);
 
             for (int i = 0; i < 500; i++)
             {          
                 progressBar1.PerformStep();
+                text += "INTERRUPT: " + (i + 1).ToString();
                 GenerateSeq();
                 currentFaults = 0;
+                //Shifting all counters.
                 for(int j = 0; j < pagesInMem; j++)
                 {
                     memory[j].Reference = false;
@@ -111,23 +126,30 @@ namespace ProjectOS
 
                 for (int j = 0; j < 20; j++)
                 {
+                    
                     Page callingPage = disk[sequence[j]].Copy();
+                    //The callingPage exists in memory.
                     if (callingPage.Position != -1)
                     {
+                        //Update info.
                         memory[callingPage.Position].Reference = true;
                         memory[callingPage.Position].ShiftCounter();
                         memory[callingPage.Position].Counter[0] = true;
                         memory[callingPage.Position].LastReferenceTime = currentTime;
                     }
+                    //The callingPage doesn't exist in memory.
                     else
                     {
                         int indexMem;
+                        //Memory isn't full.
                         if (pagesInMem < 32)
                         {
                             indexMem = pagesInMem++;
                         }
+                        //Memory is full.
                         else
                         {
+                            //Find minimum counter, replace and update info on disk.
                             indexMem = FindMin().Position;
                             int indexDisk = memory[indexMem].ID;
                             disk[indexDisk] = memory[indexMem].Copy();
@@ -135,7 +157,7 @@ namespace ProjectOS
                             disk[indexDisk].Counter.SetAll(false);
                             disk[indexDisk].Position = -1;
                         }
-
+                        //Update in on memory.
                         memory[indexMem] = callingPage.Copy();
                         memory[indexMem].Reference = true;
                         memory[indexMem].ShiftCounter();
@@ -149,6 +171,7 @@ namespace ProjectOS
                     }
                 }
 
+                //Display info.
                 #region display
                 for (int j = 0; j < pagesInMem; j++)
                 {
@@ -161,37 +184,46 @@ namespace ProjectOS
                     }
                     text += "FRAME: " + j.ToString() +
                             "\t ID: " + memory[j].ID.ToString() +
-                            "\t REFERENCE BIT: " + memory[j].Reference.ToString() +
-                            "\t LOAD TIME: " + memory[j].LoadTime.ToString() +
-                            "\t LAST REFERENCE TIME: " + memory[j].LastReferenceTime.ToString() +
-                            "\t COUNTER: " + bitCounter +
+                            "\t\t REFERENCE BIT: " + memory[j].Reference.ToString() +
+                            "\t\t LOAD TIME: " + memory[j].LoadTime.ToString() +
+                            "\t\t LAST REFERENCE TIME: " + memory[j].LastReferenceTime.ToString() +
+                            "\t\t COUNTER: " + bitCounter +
                             Environment.NewLine;
                 }
 
-                if (pagesInMem == 20)
+                if (pagesInMem < 32)
                 {
-                    for(int k = 20; k < 32; k++)
+                    for(int k = pagesInMem; k < 32; k++)
                     {
                         text += "FRAME: " + k.ToString() +
                                 "\t ID: XX" +
-                                "\t REFERENCE BIT: XXXX" + 
-                                "\t LOAD TIME: XXXX" +
-                                "\t LAST REFERENCE TIME: XXXX" +
-                                "\t COUNTER: XXXXXXXX" +
+                                "\t\t REFERENCE BIT: XXXX" +
+                                "\t\t LOAD TIME: XXXX" +
+                                "\t\t LAST REFERENCE TIME: XXXX" +
+                                "\t\t COUNTER: XXXXXXXX" +
                                 Environment.NewLine;
                     }
                 }
                 text += Environment.NewLine +
                         "\t\t CURRENT FAULTS: " + currentFaults.ToString() +
                         "\t\t TOTAL FAULTS: " + totalFaults.ToString() +
+                        Environment.NewLine +
                         Environment.NewLine;
+
+                //Clear text every 50 interrupts.
+                if (i % 50 == 0)
+                {
+                    richTextBox1.Text += text;
+                    text = String.Empty;
+                }
                 #endregion
 
                 currentTime++;
             }
-
-            richTextBox1.Text = text;
+            richTextBox1.Text += text;
+            StartBtn.Enabled = true;
             label1.Text = "TOTAL FAULTS: " + totalFaults.ToString();
         }
+
     }
 }
